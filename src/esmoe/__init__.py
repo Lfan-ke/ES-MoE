bf2ebe23a2331c2ddcedd278fef71483b3278eae
Leo Cheng (heke1228) <chengkelfan@qq.com>
2026-08-29 18:14:27 +0900

    chore(esmoe): cut 0.1.0 metadata and add CI plus trusted-publishing release workflow.
    Signed-off-by: Leo Cheng (heke1228) <chengkelfan@qq.com>

--- src/esmoe/__init__.py
+++ src/esmoe/__init__.py
@@ -4,4 +4,4 @@ from .inject import attach_aux_loss, inject_esmoe
 from .module import ESMoE
 
 __all__ = ["ESMoE", "attach_aux_loss", "clear_aux_loss", "collect_aux_loss", "graft", "inject_esmoe"]
-__version__ = "0.1.0.dev0"
+__version__ = "0.1.0"
